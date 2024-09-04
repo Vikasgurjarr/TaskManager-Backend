3ab344187a96b0f0ad35ad8a7474daf8b07dcb28
Vikasgurjarr <gurjaravs@gmail.com>
2024-09-04 16:05:37 +0530

    success deploy

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const serverless = require('serverless-http');
-
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Simple Express Function!');
+const router = express.Router();
+router.get('/', (req, res) => {
+res.send('App is running..');
 });
-
+app.use('/.netlify/functions/server', router);
 module.exports.handler = serverless(app);
+//const port = 8080;
+//app.listen(process.env.PORT || port, () => {
+//	console.log(`Listening on port ${port}`);
+//});
