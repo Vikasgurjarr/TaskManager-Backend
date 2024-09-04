ee4ea43dc58f7c5cd060099442e464493221db81
Vikasgurjarr <gurjaravs@gmail.com>
2024-09-04 17:24:47 +0530

    changes

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,53 +2,43 @@ require("dotenv").config();
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
-
 const connectdb = require("../../utils/db.js");
-const routers = require("../../routes/auth.js");
-const projectRoutes = require("../../routes/project.js");
 const errorMiddleware = require("../../middlewares/error.js");
-const adminRoute = require("../../routes/admin.js");
-const taskRoutes = require("../../routes/task.js");
-const projectController = require('../../controllers/project.js');
-
-
-const router = express.Router();
+const routes = require("../../routes/route-index.js");
 
 const app = express();
+const router = express.Router();
 
 // CORS configuration
 const corsOptions = {
-    origin: "http://localhost:3000", // You might need to update this depending on your Netlify frontend domain
-    methods: "GET,POST,PUT,PATCH,DELETE,HEAD",
-    credentials: true,
+  origin: "http://localhost:3000", // Update this based on your frontend domain
+  methods: "GET,POST,PUT,PATCH,DELETE,HEAD",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-// Mount the routers
-app.use("/api/auth", routers);
-app.use("/api/admin", adminRoute);
-app.use("/api/projects", projectRoutes);
-app.use("/api", taskRoutes);
+// Mount the routers using the router instance
+router.use("/api/auth", routes.authRoute);
+router.use("/api/admin", routes.adminRoute);
+router.use("/api/projects", routes.projectRoute);
+router.use("/api", routes.taskRoute);
 
-app.use(errorMiddleware);
+// Error middleware
+router.use(errorMiddleware);
 
 // Connect to the database
 connectdb()
-    .then(() => {
-        console.log("Database connected successfully");
-    })
-    .catch((error) => {
-        console.error("Failed to connect to the database:", error);
-    });
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
 
-    router.get('/', (req, res) => {
-      res.send('App is running..');
-      });
+// Mount the router under the base path for Netlify
+app.use("/.netlify/functions/server", router);
 
-      router.get('/projects', projectController.getProjects);
-
-      app.use('/.netlify/functions/server', router);
 // Use the express app as a serverless function
 module.exports.handler = serverless(app);
---
+++ routes/route-index.js
@@ -0,0 +1,13 @@
+
+
+const adminRoute = require("./admin");
+const authRoute = require("./auth");
+const projectRoute = require("./project");
+const taskRoute = require("./task");
+
+module.exports = {
+    adminRoute,
+    authRoute,
+    projectRoute,
+    taskRoute
+}
