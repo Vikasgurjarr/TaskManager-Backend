460ee94e1f1bf7d34185c2948cedb0148501bd73
Vikasgurjarr <gurjaravs@gmail.com>
2024-09-04 12:12:02 +0530

    changes

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,55 +1,11 @@
-
 const express = require('express');
 const serverless = require('serverless-http');
+
 const app = express();
 
-require("dotenv").config();
-const cors = require("cors");
-const router = require("./routes/auth.js");
-const connectdb = require("./utils/db.js");
-const projectRoutes = require("./routes/project.js");
-const errorMiddleware = require("./middlewares/error.js");
-const adminRoute = require("./routes/admin.js");
-const taskRoutes = require("./routes/task.js");
-
-// CORS configuration
-const corsOptions = {
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,PATCH,DELETE,HEAD",
-    credentials: true,
-};
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-// Mount the routers
-app.use("/api/auth", router);
-
-// admin route
-app.use("/api/admin",adminRoute);
-
-//project route
- app.use("/api", projectRoutes);
-
- //task route
- app.use('/api', taskRoutes);
-
- 
-app.use(errorMiddleware);
-
-const PORT = 5001;
-
-connectdb()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running at port : ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error("Failed to connect to the database:", error);
-    });
-
-
-
+// Your Express app setup
+app.get('/', (req, res) => {
+  res.send('Hello from Netlify!');
+});
 
 module.exports.handler = serverless(app);
